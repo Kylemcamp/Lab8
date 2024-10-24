c5e0767975280260c99625a0b25caf2343f2a9cc
Kyle Campbell <kmcampb1@ualberta.ca>
2024-10-24 17:56:10 -0600

    updated add to addcity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -23,7 +23,7 @@ public class CustomListTest {
         int expectedCountBeforeAdd = 0;
         assertEquals(expectedCountBeforeAdd, list.getCount(), "Count should have been 0");
         City cityToAdd = new City("Nanton", "AB");
-        list.add(cityToAdd);
+        list.addCity(cityToAdd);
 
         int expectedCountAfter = 1;
         assertEquals(expectedCountAfter, list.getCount(), "Count should have been 1");
